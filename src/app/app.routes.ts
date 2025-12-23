933a372dcc4b2b361eb1f47885cbdc7ae71a0bd9
Garbi-Collector <uwurulos@gmail.com>
2025-12-23 20:22:24 -0300

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { SidebarLayoutComponent } from './layout/sidebar-layout/sidebar-layout.component';
+
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { VentaComponent } from './pages/venta/venta.component';
+import { StockComponent } from './pages/stock/stock.component';
+import { HistorialComponent } from './pages/historial/historial.component';
+import { ConfiguracionComponent } from './pages/configuracion/configuracion.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: SidebarLayoutComponent,
+    children: [
+      { path: 'inicio', component: InicioComponent },
+      { path: 'venta', component: VentaComponent },
+      { path: 'stock', component: StockComponent },
+      { path: 'historial', component: HistorialComponent },
+      { path: 'configuracion', component: ConfiguracionComponent },
+
+      // ruta por defecto
+      { path: '', redirectTo: 'inicio', pathMatch: 'full' }
+    ]
+  }
+];
