5a1726963f6545199b8b06dc836bfc6bf0b45654
Garbi-Collector <uwurulos@gmail.com>
2025-12-25 02:04:30 -0300

    API fix

--- main.js
+++ main.js
@@ -19,6 +19,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    frame: false,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     },
@@ -42,6 +43,29 @@ function createWindow () {
     mainWindow = null;
   });
 }
+
+
+const { ipcMain } = require('electron');
+
+ipcMain.on('window:minimize', () => {
+  if (mainWindow) mainWindow.minimize();
+});
+
+ipcMain.on('window:maximize', () => {
+  if (!mainWindow) return;
+
+  if (mainWindow.isMaximized()) {
+    mainWindow.unmaximize();
+  } else {
+    mainWindow.maximize();
+  }
+});
+
+ipcMain.on('window:close', () => {
+  if (mainWindow) mainWindow.close();
+});
+
+
 
 app.on('ready', createWindow);
 
