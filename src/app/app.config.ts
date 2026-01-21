145a90ccd600931d54b1ba83d7babd3c29184b11
Garbi-Collector <uwurulos@gmail.com>
2026-01-21 13:34:36 -0300

    routes fix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,18 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import { provideRouter, withRouterConfig } from '@angular/router';
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
-import {provideHttpClient} from "@angular/common/http";
-
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withRouterConfig({
+        onSameUrlNavigation: 'reload'
+      })
+    ),
     provideHttpClient(),
-    provideClientHydration()]
+    provideClientHydration()
+  ]
 };
